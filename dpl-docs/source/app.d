9bfe1d6ccf540c3c19ea07559d3db316223bb363
Sönke Ludwig <sludwig@rejectedsoftware.com>
2013-02-21 00:14:58 +0100

    Support --git-target to specify the tag/branch to use for linking to source code.

--- dpl-docs/source/app.d
+++ dpl-docs/source/app.d
@@ -1,10 +1,16 @@
 module app;
 
 import ddox.main;
+import std.getopt;
+import std.process;
 import vibe.core.log;
 
 int main(string[] args)
 {
+	string git_target = "master";
+	getopt(args, std.getopt.config.passThrough,
+		"git-target", &git_target);
+	environment["GIT_TARGET"] = git_target;
 	setPlainLogging(true);
 	return ddoxMain(args);
 }
