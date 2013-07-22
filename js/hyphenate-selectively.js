6dd0150ab2673abbfac2c8411845949c182b79e4
Brad Anderson <eco@gnuk.net>
2013-07-22 15:32:48 -0600

    Remove hyphenate.js

--- js/hyphenate-selectively.js
+++
@@ -1,7 +0,0 @@
-// CSS hyphens is supported by IE10+, Firefox 6+, and Safari 5.1+ so only
-// use hyphenator.js for Chrome where support is still missing completely (as
-// of Chrome 29) and Opera (which now uses Chrome's rendering engine Blink).
-var re = /(Chrome|Opera)/;
-if (re.test(navigator.userAgent)) {
-    document.write('<script src="/js/hyphenate.js" type="text/javascript"></script>');
-}
