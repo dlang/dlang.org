82aef1d09c46c60b7688dc65ad8e97dc22110013
Sönke Ludwig <sludwig@rejectedsoftware.com>
2014-05-16 10:59:25 +0200

    Update to DDOX 0.9.19 with multiple declarations per page support.
     - Fixes the code that implants a link into the page title
     - Includes dub.selections.json, to make sure everyone uses the same dependencies.

--- dpl-docs/source/app.d
+++ dpl-docs/source/app.d
@@ -11,6 +11,6 @@ int main(string[] args)
 	getopt(args, std.getopt.config.passThrough,
 		"git-target", &git_target);
 	environment["GIT_TARGET"] = git_target;
-	setPlainLogging(true);
+	setLogFormat(FileLogger.Format.plain);
 	return ddoxMain(args);
 }
