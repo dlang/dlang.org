127ef0c5f535625c1844f34d1c8b2757018ca495
Nick Treleaven <nick.treleaven@btinternet.com>
2014-12-19 18:09:17 +0000

    Use hierarchical Jump-to links
    Show non-top-level jump-to links even when inhibitQuickIndex is set.
    Show all jump-to links with same background color as symbol
    declarations.

--- js/listanchors.js
+++ js/listanchors.js
@@ -8,43 +8,64 @@ License:   http://boost.org/LICENSE_1_0.txt, Boost License 1.0
 Authors:   Andrei Alexandrescu, Nick Treleaven
 */
 
-/* Symbol definition */
-var lastSymbolDefined = '';
-var values = [];
-//
 function lastName(a) {
     var pos = a.lastIndexOf('.');
     return a.slice(pos + 1);
 }
-//
-function defineSymbol(sym) {
-  // ignore ditto overloads
-  if (sym == lastSymbolDefined) return;
-  // ignore foo.bar, foo.bar.2, but show foo, foo.2
-  var pos = sym.indexOf('.');
-  if (pos >= 0 && (sym.lastIndexOf('.') != pos || isNaN(lastName(sym)))) return;
-  lastSymbolDefined = sym;
-  values.push(sym);
-}
-//
+
 function listanchors()
 {
-    if (typeof inhibitQuickIndex !== 'undefined') return;
+    var hideTop = (typeof inhibitQuickIndex !== 'undefined');
     var a = document.getElementById("quickindex");
     if (!a) return;
 
-    values.sort();
-
-    var newText = "";
-    for (var i = 0; i < values.length; i++) {
-        var a = values[i];
-        var text = a;
-        if (i != 0) newText += " &middot;"; 
-        newText += ' <a class="jumpto" href="#.' + a +
-            '"><span class="notranslate donthyphenate">' + text + '</span></a>';
+    // build hash of parent anchor names -> array of child anchor names
+    var parentNames = [];
+    var lastAnchor = '';
+    var items = document.getElementsByClassName('quickindex');
+    for (var i = 0; i < items.length; i++)
+    {
+        var text = items[i].id;
+        // ignore top-level quickindex
+        var pos = text.indexOf('.');
+        if (pos < 0) continue;
+        // skip 'quickindex'
+        text = text.slice(pos);
+        // ignore any ditto overloads (which have the same anchor name)
+        if (text == lastAnchor) continue;
+        lastAnchor = text;
+        
+        var pos = text.lastIndexOf('.');
+        if (hideTop && pos == 0) continue;
+        var parent = (pos == 0) ? '' : text.slice(0, pos);
+        
+        if (!parentNames[parent])
+            parentNames[parent] = [text];
+        else
+            parentNames[parent].push(text);
     }
-    values = null; // unreference memory
-    if (newText != "") newText = "<p><b>Jump to:</b>" + newText + "</p>";
-    var a = document.getElementById("quickindex");
-    a.innerHTML = newText;
+    // populate quickindex elements
+    for (var key in parentNames)
+    {
+        var arr = parentNames[key];
+        // we won't display the qualifying names to save space, so sort by last name
+        arr.sort(function(a,b){
+            var aa = lastName(a).toLowerCase();
+            var bb = lastName(b).toLowerCase();
+            return aa == bb ? 0 : (aa < bb ? -1 : 1);
+        });
+        var newText = "";
+        for (var i = 0; i < arr.length; i++) {
+            var a = arr[i];
+            var text = lastName(a);
+            if (i != 0) newText += " &middot;"; 
+            newText += ' <a class="jumpto" href="#' + a +
+                '"><span class="notranslate donthyphenate">' + text + '</span></a>';
+        }
+        if (newText != "") newText = "<p><b>Jump to:</b>" + newText + "</p>";
+        var id = 'quickindex';
+        id += key;
+        var e = document.getElementById(id);
+        e.innerHTML = newText;
+    }
 }
