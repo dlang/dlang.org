6bc754b14a5edfc938381b9b6ffe16d25c210350
Nick Treleaven <nick.treleaven@btinternet.com>
2015-01-06 18:09:32 +0000

    Remove ddoc workarounds

--- js/listanchors.js
+++ js/listanchors.js
@@ -23,8 +23,7 @@ function defineSymbol(sym) {
   // ignore foo.bar, foo.bar.2, but show foo, foo.2
   var pos = sym.indexOf('.');
   if (pos >= 0 && (sym.lastIndexOf('.') != pos || isNaN(lastName(sym)))) return;
-  // Note: empty comment avoids ddoc macro definition
-  /**/ lastSymbolDefined = sym;
+  lastSymbolDefined = sym;
   values.push(sym);
 }
 //
@@ -41,11 +40,11 @@ function listanchors()
         var a = values[i];
         var text = a;
         if (i != 0) newText += " &middot;"; 
-        newText += ' \x3Ca class="jumpto" href="\x23.' + a +
-            '"\x3E\x3Cspan class="notranslate donthyphenate"\x3E' + text + '\x3C/span\x3E\x3C/a\x3E';
+        newText += ' <a class="jumpto" href="#.' + a +
+            '"><span class="notranslate donthyphenate">' + text + '</span></a>';
     }
-    /**/ values = null; // unreference memory
-    if (newText != "") newText = "\x3Cp\x3E\x3Cb\x3EJump to:\x3C/b\x3E" + newText + "\x3C/p\x3E";
+    values = null; // unreference memory
+    if (newText != "") newText = "<p><b>Jump to:</b>" + newText + "</p>";
     var a = document.getElementById("quickindex");
     a.innerHTML = newText;
 }
