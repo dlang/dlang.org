176c3a7d6ca277eff6bca5a9a1db81813ad6c197
Vladimir Panteleev <vladimir@thecybershadow.net>
2015-05-25 09:53:27 +0000

    js/dlang.js: Add cssmenu_no_js hook

--- js/dlang.js
+++ js/dlang.js
@@ -1,51 +1,53 @@
 (function($) {
 	$(function() {
-		var menu_ul = $('#cssmenu > ul > li > ul');
-		menu_ul.hide();
+		if (typeof cssmenu_no_js === 'undefined') {
+			var menu_ul = $('#cssmenu > ul > li > ul');
+			menu_ul.hide();
 
-		function baseName(str) {
-			return str.split('/').pop();
+			function baseName(str) {
+				return str.split('/').pop();
+			}
+
+			// highlight menu entry of the current page
+			var href = window.location.href.split('#')[0];
+			var current;
+			var res = $('#cssmenu a').each(function (_, a) {
+				if (a.href == href) {
+					current = a;
+					return false;
+				}
+			});
+			current = $(current);
+			// direct li parent containing the link
+			current.parent('li').addClass('active');
+			// topmost li parent, e.g. 'std'
+			current.parents('#cssmenu li.has-sub').addClass('active')
+			// show menu tree
+				.children('ul').show();
+
+
+			$('#cssmenu > ul > li > a').click(function() {
+				$li = $(this).closest('li');
+				if (!$li.hasClass('has-sub')) {
+					$('#cssmenu li').removeClass('active');
+				}
+				$li.addClass('active');
+				var checkElement = $(this).next();
+				if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+					$(this).closest('li').removeClass('active');
+					checkElement.slideUp('normal');
+				}
+				if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+					/* $('#cssmenu ul ul:visible').slideUp('normal'); */
+					checkElement.slideDown('normal');
+				}
+				if($(this).closest('li').find('ul').children().length == 0) {
+					return true;
+				} else {
+					return false;
+				}
+			});
 		}
-
-		// highlight menu entry of the current page
-		var href = window.location.href.split('#')[0];
-		var current;
-		var res = $('#cssmenu a').each(function (_, a) {
-			if (a.href == href) {
-				current = a;
-				return false;
-			}
-		});
-		current = $(current);
-		// direct li parent containing the link
-		current.parent('li').addClass('active');
-		// topmost li parent, e.g. 'std'
-		current.parents('#cssmenu li.has-sub').addClass('active')
-		// show menu tree
-			.children('ul').show();
-
-
-		$('#cssmenu > ul > li > a').click(function() {
-			$li = $(this).closest('li');
-			if (!$li.hasClass('has-sub')) {
-				$('#cssmenu li').removeClass('active');
-			}
-			$li.addClass('active');
-			var checkElement = $(this).next();
-			if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
-				$(this).closest('li').removeClass('active');
-				checkElement.slideUp('normal');
-			}
-			if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
-				/* $('#cssmenu ul ul:visible').slideUp('normal'); */
-				checkElement.slideDown('normal');
-			}
-			if($(this).closest('li').find('ul').children().length == 0) {
-				return true;
-			} else {
-				return false;
-			}
-		});
 
 		$('<div>')
 			.attr('id', 'mobile-hamburger')
