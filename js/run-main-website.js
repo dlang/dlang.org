be3448577f9f3156a73c0bfb44eb4d0abf682f68
Vladimir Panteleev <vladimir@thecybershadow.net>
2015-09-12 18:02:32 +0000

    run-main-website.js: Add/update example inputs

--- js/run-main-website.js
+++ js/run-main-website.js
@@ -9,11 +9,17 @@ Authors:   Damian Ziemba
 */
 var mainPage = new Array();
 
-//The top one
-mainPage["27d33ceb10933b5541f5447f4bf1fc17"] = ["The D programming language. Modern convenience. Modeling power. Native efficiency."];
+// Top rotation - sort lines
+mainPage["eaed85820b01767abc1e1da8a35ebd12"] = ["The D programming language.\nModern convenience.\nModeling power.\nNative efficiency."];
 
-//Automatic memory management makes for safe..
+// Top rotation - RPN calculator
+mainPage["9386a20f75d1fd4281ae6c6d6e645705"] = ["3 4 +\n1 2 + 4 * 5 + 3 -\n2 3 7 * *"];
+
+// Top rotation - round floating point numbers
+mainPage["3d41a91e2b9e27835ab8d6c73367a909"] = ["2.4 plus 2.4 equals 5 for sufficiently large values of 2."];
+
+// Automatic memory management makes for safe...
 mainPage["54f2095e83f8ef851a711db0a29b0e26"] = ["", "text.txt"];
 
-//Built-in linear and associative arrays, slices, and ranges make daily programmin
-mainPage["6c843684c0231b847f933e6d7b5a697a"] = ["The D programming language. Modern convenience. Modeling power. Native efficiency."];
+// Built-in linear and associative arrays, slices, and ranges make...
+mainPage["6c843684c0231b847f933e6d7b5a697a"] = ["The D programming language.\nModern convenience.\nModeling power.\nNative efficiency."];
