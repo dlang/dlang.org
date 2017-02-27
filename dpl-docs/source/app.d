16c6bc44ffd06b01cafac8bb9735bf6265276ac0
Sebastian Wilzbach <seb@wilzba.ch>
2017-02-27 09:59:31 +0100

    Support --no-exact-source-links in dpl-docs

--- dpl-docs/source/app.d
+++ dpl-docs/source/app.d
@@ -5,12 +5,16 @@ import std.getopt;
 import std.process;
 import vibe.core.log;
 
+bool noExactSourceCodeLinks;
+
 int main(string[] args)
 {
 	string git_target = "master";
 	getopt(args, std.getopt.config.passThrough,
-		"git-target", &git_target);
+		"git-target", &git_target,
+		"no-exact-source-links", &noExactSourceCodeLinks);
 	environment["GIT_TARGET"] = git_target;
+	environment["NO_EXACT_SOURCE_CODE_LINKS"] = noExactSourceCodeLinks ? "1" : "0";
 	setLogFormat(FileLogger.Format.plain);
 	return ddoxMain(args);
 }
