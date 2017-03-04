bac6b21591ad085c444cf51d67c5d9b17b61d7a9
Martin Nowak <code@dawg.eu>
2017-03-04 16:46:57 +0100

    only add release versions to index

--- changelog/update_nav.sh
+++ changelog/update_nav.sh
@@ -28,14 +28,15 @@ done
 
 # reverse sort versions array, http://stackoverflow.com/a/11789688/2371032
 IFS=$'\n'
-rev_vers=($(sort --reverse <<<"${all_vers[*]}"))
+rev_all_vers=($(sort --reverse <<<"${all_vers[*]}"))
+rev_rel_vers=($(sort --reverse <<<"${rel_vers[*]}"))
 unset IFS
 
 # update index of all changlogs
 sed -i '/BEGIN_GENERATED_CHANGELOG_VERSIONS/,/END_GENERATED_CHANGELOG_VERSIONS/d' changelog.ddoc
 echo '_=BEGIN_GENERATED_CHANGELOG_VERSIONS' >> changelog.ddoc
 echo 'CHANGELOG_VERSIONS =' >> changelog.ddoc
-for ver in "${rev_vers[@]}"; do
+for ver in "${rev_rel_vers[@]}"; do
     echo "    \$(CHANGELOG_VERSION ${ver%.dd})" >> changelog.ddoc
 done
 echo '_=END_GENERATED_CHANGELOG_VERSIONS' >> changelog.ddoc
