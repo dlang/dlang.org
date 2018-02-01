ba15cf9802f69fda4d20bfebe87238884d45f076
Sebastian Wilzbach <seb@wilzba.ch>
2018-02-01 08:57:50 +0100

    Add nightly (aka pending) changelog to the changelog navigation menu

--- changelog/update_nav.sh
+++ changelog/update_nav.sh
@@ -48,6 +48,7 @@ unset IFS
 sed -i '/BEGIN_GENERATED_CHANGELOG_VERSIONS/,/END_GENERATED_CHANGELOG_VERSIONS/d' changelog.ddoc
 echo '_=BEGIN_GENERATED_CHANGELOG_VERSIONS' >> changelog.ddoc
 echo 'CHANGELOG_VERSIONS =' >> changelog.ddoc
+echo '    $(CHANGELOG_VERSION_NIGHTLY)' >> changelog.ddoc
 for ver in "${rev_pre_vers[@]}"; do
     echo "    \$(CHANGELOG_VERSION_PRE ${ver%_pre.dd}, not yet released)" >> changelog.ddoc
 done
