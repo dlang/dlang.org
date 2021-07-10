bc10c3fe8c9c4f411f2ec7bec87caf2bebdc4fd5
Max Haughton <maxhaton@gmail.com>
2021-07-10 22:50:50 +0100

    Make build script less noisy, integrate with makefile using 6 jobs (Takes next to no time for me)

--- book/build.d
+++ book/build.d
@@ -1,19 +1,19 @@
 module book.build;
 import std;
-void compileToPath(string compileThis, string outputPath)
+void compileToPath(string compileThis, string outputPath, bool loud = false)
 {
     import core.stdc.stdlib;
     if(outputPath.indexOf("cozum") == -1)
     {
-        writeln("wow");
-        writeln(outputPath.replace(".html", ".cozum.html"));
+        if(loud)
+            writeln(outputPath.replace(".html", ".cozum.html"));
         //exit(0);
     }
     
     const compileString = format!"dmd -revert=markdown -D  macros.ddoc html.ddoc dlang.org.ddoc doc.ddoc aliBook.ddoc %s -Df%s "(compileThis, outputPath);
-    writefln!"%s:%s |> %s"(compileThis, outputPath, compileString);
+    if(loud)
+        writefln!"%s:%s |> %s"(compileThis, outputPath, compileString);
     const res = executeShell(compileString);
-    writeln("\t\t", res.status);
     
     if(res.status != 0) {
         write(res.output);
@@ -21,8 +21,9 @@ void compileToPath(string compileThis, string outputPath)
         exit(0);
     }
 }
-int main()
+int main(string[] args)
 {
+    const jobs = args.length == 2 ? args[1].to!ubyte : 1;
     const outDir = "../web/book";
     writeln("Building the book at ", outDir);
     //scope(failure) return 1;
@@ -31,13 +32,11 @@ int main()
     if(outDir.exists) {
         executeShell("rm -rf " ~ outDir);
     }
-    writeln("Building book files");
     auto rng = dirEntries("d.en", "*.d", SpanMode.shallow)
         .map!(dFile => tuple(dFile.name, buildPath(outDir, baseName(dFile).setExtension("html"))))
-        //.parallel(1)
+        .parallel(jobs)
         .each!(elem => compileToPath(elem.tupleof));
     dirEntries("d.en", "*.png", SpanMode.shallow)
         .each!(p => copy(p, buildPath(outDir, baseName(p).setExtension("png"))));
-    writeln("Finished");
     return 0;
 }
