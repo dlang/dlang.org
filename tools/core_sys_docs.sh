364e87d66aab4e14c5c6fa8509e58b36332a429c
Nick Treleaven <ntrel002@gmail.com>
2026-03-02 22:41:57 +0000

    Detect nested doc-comments and line-comments, ignore `T**`

--- tools/core_sys_docs.sh
+++ tools/core_sys_docs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # list all modules in $1 which have more than one doc-comment
 find $1 -name '*.d' \
-    | xargs grep -Fc '**' \
+    | xargs grep -Ec '/\*\*|/\+\+|///' \
     | grep -vE ':[01]$' \
     | awk -F '/src/' '{ print $2 }' \
     | awk -F '.d:' '{ print $1 }' \
